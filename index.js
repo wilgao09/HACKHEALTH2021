53cbb043d769decb98cf1bd29381e027b1086ea6
wilgao09 <william.gao09@gmail.com>
2021-02-13 07:35:34 -0500

    Added FontControl

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
+
+var EXT_NAME = "my extension name"
+
+
 let Dimmer = {
-  inputElement: null,
-  sendUpdate: () => {
-    let currVal = Dimmer.inputElement.value;
-    sendJSONToTab({ src: "MYEXT", from: "DIMMER", val: currVal });
-  },
-};
+    inputElement: null,
+    sendUpdate: () => {
+        let currVal = Dimmer.inputElement.checked
+        sendJSONToTab({src:EXT_NAME,from:"DIMMER", val:currVal})
+    },
+}
+
+let FontControl = {
+    inputElement: null,
+    sendUpdate: () => {
+        let currVal = FontControl.inputElement.value
+        sendJSONToTab({src:EXT_NAME,from:"FONT_CONTROL", val:currVal})
+    }
+}
+
+
+
+
+
 
 function init() {
-  var DIMMER_ELEMENT = document.getElementById("dim");
 
-  Dimmer.inputElement = DIMMER_ELEMENT;
-  DIMMER_ELEMENT.addEventListener("change", () => {
-    Dimmer.sendUpdate();
-  });
+    var DIMMER_ELEMENT = document.getElementById("dim");
+    Dimmer.inputElement = DIMMER_ELEMENT;
+    DIMMER_ELEMENT.addEventListener("change", () => {
+        Dimmer.sendUpdate();
+    })
+
+    var FONT_ELEMENT = document.getElementById("fontval");
+    FontControl.inputElement = FONT_ELEMENT;
+    FONT_ELEMENT.addEventListener("change", () => {
+        FontControl.sendUpdate()
+    })
+
+
 }
 
-document.addEventListener("DOMContentLoaded", init);
 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+
+
+
+document.addEventListener("DOMContentLoaded", init)
+
+
+
+function myFunction(){
+	document.getElementById("myDropdown").classList.toggle("show");
 }
-window.onClick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.detElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
+window.onClick = function(event){
+	if(!event.target.matches('.dropbtn')){
+		var dropdowns = document.detElementsByClassName("dropdown-content");
+		var i;
+		for(i = 0;i < dropdowns.length;i++){
+			var openDropdown = dropdowns[i];
+			if(openDropdown.classList.contains('show')){
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
